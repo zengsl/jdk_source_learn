f63d0227d0ff76fecd2fd51681c09ee9ad940d90
zengsl <zengsl0212@foxmail.com>
2021-05-11 17:34:28 +0800

    java8 new feature train

--- src/main/java/com/zzz/test/feature/ifunction/Car.java
+++ src/main/java/com/zzz/test/feature/ifunction/Car.java
@@ -10,7 +10,7 @@ import java.util.function.Supplier;
 public class Car {
 
     /**
-     * Supplier是jdk1.8的接口，这里和lamda一起使用了
+     * Supplier是jdk1.8的接口，这里和lambda一起使用了
      */
     public static Car create(final Supplier<Car> supplier) {
         return supplier.get();
